e0ec736b3ce2065cc4b2013cb252f2621e17883a
sista4788 <63381022+sista4788@users.noreply.github.com>
2020-11-09 12:39:51 -0500

    del del file

---
+++ DelDel.java
@@ -0,0 +1,14 @@
+// Given a string, if the string "del" appears starting at index 1, return a string where that "del" has been deleted. Otherwise, return the string unchanged.
+
+// delDel("adelbc") → "abc"
+// delDel("adelHello") → "aHello"
+// delDel("adedbc") → "adedbc"
+
+public class DelDel {
+    public String delDel(String str) {
+        if (str.length() >=4 && str.substring(1,4).equals("del")){
+          return str.charAt(0) + str.substring(4,str.length());
+        }
+        return str;
+      }
+}
